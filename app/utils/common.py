8b2359fd4d2a9bced512d54b702c0f86e616c936
wg99 <146656013+wg99nj@users.noreply.github.com>
2024-11-19 18:29:04 -0500

    fixing

--- app/utils/common.py
+++ app/utils/common.py
@@ -2,11 +2,11 @@ import logging.config
 import os
 import base64
 from typing import List
-from dotenv import load_dotenv
-from jose import jwt
+from dotenv import load_dotenv # type: ignore
+from jose import jwt # type: ignore
 from datetime import datetime, timedelta
 from app.config import ADMIN_PASSWORD, ADMIN_USER, ALGORITHM, SECRET_KEY
-import validators  # Make sure to install this package
+import validators  # type: ignore # Make sure to install this package
 from urllib.parse import urlparse, urlunparse
 
 # Load environment variables from .env file for security and configuration.
@@ -66,7 +66,7 @@ def encode_url_to_filename(url):
     Encodes a URL into a base64 string safe for filenames, after validating and sanitizing.
     Removes padding to ensure filename compatibility.
     """
-    sanitizd_url = validate_and_sanitize_url(str(url))
+    sanitized_url = validate_and_sanitize_url(str(url))
     if sanitized_url is None:
         raise ValueError("Provided URL is invalid and cannot be encoded.")
     encoded_bytes = base64.urlsafe_b64encode(sanitized_url.encode('utf-8'))
@@ -81,7 +81,7 @@ def decode_filename_to_url(encoded_str: str) -> str:
     padding_needed = 4 - (len(encoded_str) % 4)
     if padding_needed:
         encoded_str += "=" * padding_needed
-    decoded_bytes = base64.urlsafe_b6decode(encoded_str)
+    decoded_bytes = base64.urlsafe_b64decode(encoded_str)
     return decoded_bytes.decode('utf-8')
 
 def generate_links(action: str, qr_filename: str, base_api_url: str, download_url: str) -> List[dict]:
